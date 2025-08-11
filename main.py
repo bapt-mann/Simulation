66dc41d7d222f74c570ddd0819fa0a1f601d20b2
Batakuu <mrblnz26@gmail.com>
2025-08-11 23:59:49 +0200

    modify main file

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import pygame
+import random
 from Field import Field  # Import direct de la classe
 from Block import Block
 
 pygame.init()
+clock = pygame.time.Clock()
 
 running = True
 field = Field(800, 600)  # Création d'une instance avec taille personnalisée
-block = Block(10, 10, 100, 100)
+block_list = []
+
+for _ in range(10):
+    block = Block(15, 15, random.randint(0, 700), random.randint(0, 500), 1)
+    block_list.append(block)
+
+for _ in range(10):
+    block = Block(15, 15, random.randint(0, 700), random.randint(0, 500), 2)
+    block_list.append(block)
+
+for _ in range(10):
+    block = Block(15, 15, random.randint(0, 700), random.randint(0, 500), 0)
+    block_list.append(block)
 
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     field.screen.fill((0, 0, 0))  # Nettoyer écran avant de dessiner
-    pygame.draw.rect(field.screen, block.color, block.block)
+
+    for block in block_list:
+        block.move()
+        block.detect_collision()
+        pygame.draw.rect(field.screen, block.color, block.block)
+
     pygame.display.flip()  # Met à jour l'affichage
 
 pygame.quit()
+
+
+
 
 
 
