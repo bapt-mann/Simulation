2270653a121062789059895d1880541fcfc3e969
Batakuu <mrblnz26@gmail.com>
2025-08-15 19:36:20 +0200

    update main

--- simulation/main.py
+++ simulation/main.py
@@ -4,26 +4,29 @@ from Field import Field  # Import direct de la classe
 from Block import Block
 
 pygame.init()
+
+
 clock = pygame.time.Clock()
 
 running = True
 field = Field(720, 1000)  # Création d'une instance avec taille personnalisée
 field.screen.fill((250, 250, 250))
 
-block_size = (30, 30)  # Taille des blocs
-image_size = (50, 50)  # Taille des images
+block_size = (50, 50)  # Taille des blocs
+image_size = (80, 80)  # Taille des images
 Block.load_images(image_size)
+Block.load_sounds()
 block_list = []
 
-for _ in range(30):
+for _ in range(10):
     block = Block(block_size[0], block_size[1], random.randint(0, 300), random.randint(500, 1000), 1)
     block_list.append(block)
 
-for _ in range(30):
+for _ in range(10):
     block = Block(block_size[0], block_size[1], random.randint(400, 700), random.randint(500, 1000), 2)
     block_list.append(block)
 
-for _ in range(30):
+for _ in range(10):
     block = Block(block_size[0], block_size[1], random.randint(100, 600), random.randint(100, 400), 0)
     block_list.append(block)
 
@@ -33,7 +36,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    field.screen.fill((240, 240, 240))  # Nettoyer écran avant de dessiner
+    field.screen.fill((200,200,200))  # Nettoyer écran avant de dessiner
 
     for block in block_list:
         if Block.test:
